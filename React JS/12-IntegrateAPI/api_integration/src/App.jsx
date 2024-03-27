df1140fa2d473d4cd8dbfebc3046f2265e2163a7
frrukh <mhmdfrrukh13@gmail.com>
2024-03-27 18:00:30 +0500

    routing and API integration

---
+++ React JS/12-IntegrateAPI/api_integration/src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import axios from 'axios'
+
+function App() {
+
+const url = "https://fakestoreapi.com/products";
+  const getData = ()=>{
+    axios.get(url)
+    .then(products=>{
+      console.log(products.data)
+    }).catch(err=>{
+      console.log(err)
+    })
+  }
+
+  const sendData = ()=>{
+    axios.post(url, {
+        title: 'test product',
+        price: 13.5,
+        description: 'lorem ipsum set',
+        image: 'https://i.pravatar.cc',
+        category: 'electronic'
+    })
+    .then(resp => console.log(resp.data))
+    .catch(err=>console.log(err));
+  }
+
+  return (
+    <div className='p-40'>
+      <button onClick={getData} className='px-5 py-1 bg-sky-600 text-white rounded-lg'>Get data</button>
+      <br />
+      <br />
+      <button onClick={sendData} className='px-5 py-1 bg-sky-600 text-white rounded-lg'>Send data</button>
+      <br />
+      <br />
+    </div>
+  )
+}
+
+export default App;
